3db79bba9605bc796a95162bbe6d684ae2492aca
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-02-16 13:09:21 +0530

    typed

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package gohtmx
+package main
 
 import (
 	"html/template"
@@ -14,6 +14,25 @@ func main() {
 		tmpl.Execute(w, nil)
 	})
 
+	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("./templates/fragments/results.html"))
+		data := map[string][]Stock{
+			"Results": SearchTicker(r.URL.Query().Get("key")),
+		}
+		tmpl.Execute(w, data)
+	})
+
+	http.HandleFunc("/stock/", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case "POST":
+			ticker := r.PostFormValue("ticker")
+			stk := SearchTicker(ticker)[0]
+			val := GetDailyValues(ticker)
+			tmpl := template.Must(template.ParseFiles("./templates/index.html"))
+			tmpl.ExecuteTemplate(w, "stock-element", Stock{Ticker: stk.Ticker, Name: stk.Name, Price: val.Open})
+		}
+	})
+
 	log.Println("App running on 9090....")
-	log.Fatal((http.ListenAndServe(":9090", nil)))
+	log.Fatal(http.ListenAndServe(":9090", nil))
 }
